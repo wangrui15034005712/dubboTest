129db073e6e451dae4ce0e2c450205ed0031d6ed
wangrui <15034005712@163.com>
2018-10-31 09:58:16 +0800

    dubboDemo

---
+++ dubboConsumer2/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -0,0 +1,21 @@
+package com.alibaba.dubbo.consumer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+/**
+ * Created by wy on 2017/4/13.
+ */
+public class Consumer {
+    public static void main(String[] args) {
+
+        //测试常规服务
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        System.out.println("consumer2 start");
+        DemoService demoService = context.getBean(DemoService.class);
+        System.out.println("consumer2");
+        System.out.println(demoService.getPermissions(1L));
+    }
+}
